7932f85318e2731f2fa4da3939f4c8e53f741446
Florian GUILLEMEAU <floguild68@hotmail.fr>
2017-12-13 20:56:06 +0100

    Correction de quelques pages

--- src/BW/UtilisateurBundle/Controller/UtilisateurController.php
+++ src/BW/UtilisateurBundle/Controller/UtilisateurController.php
@@ -7,6 +7,29 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class UtilisateurController extends Controller{
 
 	public function indexAction(){
-        return $this->render('BWUtilisateurBundle:Utilisateur:utilisateur.html.twig');
+    $Recetterepository = $this->getDoctrine()
+      ->getRepository('BWRecetteBundle:Recette')
+    ;
+
+    $Imagerepository = $this->getDoctrine()
+      ->getRepository('BWRecetteBundle:Image')
+    ;
+
+    $recette = $Recetterepository->findAll();
+    $image = $Imagerepository->findAll();
+    $liste_i = array();
+    foreach($recette as $r){
+      foreach($image as $i){
+        if($i->getidrecette() == $r->getidrecette())
+          $liste_i[] = $i;
+      }
     }
+
+    $count = count($recette);
+
+    return $this->render('BWUtilisateurBundle:Utilisateur:utilisateur.html.twig', array(
+      'recette' => $recette,
+      'image' => $liste_i,
+      'count' => $count));
+  	}
 }
